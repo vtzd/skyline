678dd4c176fa891cc5a338608759afa2fc9acaf4
vtzd <vtzd.dev@gmail.com>
2024-11-26 22:17:14 +0100

    create handlers to tranform request data per exchange

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,24 +5,24 @@ import tseslint from "typescript-eslint";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    files: [ "**/*.{js,mjs,cjs,ts}" ],
+    files: ["**/*.{js,mjs,cjs,ts}"],
     rules: {
-      "array-bracket-spacing": [ "error", "always" ],
-      "object-curly-spacing": [ "error", "always" ],
-      "space-in-parens": [ "error", "always" ],
-      "max-len": [ "error", { 
+      "array-bracket-spacing": ["error", "always"],
+      "object-curly-spacing": ["error", "always"],
+      "space-in-parens": ["error", "always"],
+      "max-len": ["error", {
         "code": 80,
         "tabWidth": 2,
         "ignoreUrls": true,
         "ignoreStrings": true,
         "ignoreTemplateLiterals": true,
         "ignoreRegExpLiterals": true
-      } ],
+      }],
       "@typescript-eslint/no-explicit-any": "error",
       "@typescript-eslint/explicit-function-return-type": "error",
       "@typescript-eslint/explicit-member-accessibility": "error",
       "@typescript-eslint/explicit-module-boundary-types": "error",
-      "@typescript-eslint/no-inferrable-types": "off"
+      "@typescript-eslint/no-inferrable-types": "off",
     }
   },
   { languageOptions: { globals: globals.browser } },
--- src/index.ts
+++ src/index.ts
@@ -28,24 +28,34 @@ process.on('uncaughtException', (error) => {
   console.error('Uncaught exception:', error);
 });
 
-const VALID_SYMBOLS: string[] = ["KAS"]
+const VALID_SYMBOLS = ["KAS"] as const
+type ValidSymbol = typeof VALID_SYMBOLS[number]
 const isValidSymbol = (symbol: any): boolean => {
   if (typeof symbol !== "string") {
     return false
   }
 
-  return VALID_SYMBOLS.includes(symbol)
+  return VALID_SYMBOLS.includes(symbol as ValidSymbol)
 }
 
+const VALID_EXCHANGES = ["Kraken"] as const
+type ValidExchange = typeof VALID_EXCHANGES[number]
+const isValidExchange = (exchange: any): boolean => {
+  if (typeof exchange !== "string") {
+    return false
+  }
 
+  return VALID_EXCHANGES.includes(exchange as ValidExchange)
+}
 
-const VALID_ACTIONS: string[] = ["buy", "sell"]
+const VALID_ACTIONS = ["buy", "sell"] as const
+type ValidAction = typeof VALID_ACTIONS[number]
 const isValidAction = (action: any): boolean => {
   if (typeof action !== "string") {
     return false
   }
 
-  return VALID_ACTIONS.includes(action)
+  return VALID_ACTIONS.includes(action as ValidAction)
 }
 
 
@@ -57,34 +67,84 @@ const isValidSecret = (secret: any): boolean => {
   return process.env.API_SECRET === secret
 }
 
-type TradeParams = {
-  symbol: string,
-  action: string,
-  secret: string
+
+const isValidStopLoss = (stopLoss: any): boolean => {
+  return (
+    typeof stopLoss !== "number" ||
+    stopLoss > 100 ||
+    stopLoss <= 0
+  )
 }
-const validateTradeParams = (params: { [key: string]: any }): Omit<TradeParams, "secret"> | null => {
-  const { symbol, action, secret } = params
+
+const parseStopLoss = (stopLoss: any): number | undefined => {
+  return isValidStopLoss(stopLoss) ? stopLoss : undefined
+}
+
+type TradeParams = {
+  symbol: ValidSymbol,
+  exchange: ValidExchange,
+  action: ValidAction,
+  stopLoss?: number
+}
+const validateTradeParams = (params: { [key: string]: any }): TradeParams | null => {
+  const { symbol, exchange, action, secret, stopLoss } = params
   if (
     isValidSymbol(symbol) &&
+    isValidExchange(exchange) &&
     isValidAction(action) &&
     isValidSecret(secret)
   ) {
-    return { symbol, action }
+    return {
+      symbol,
+      exchange,
+      action,
+      stopLoss: parseStopLoss(stopLoss)
+    }
   }
 
   return null
+}
+
+
+const createKrakenRequest = (params: TradeParams): unknown => {
+  return {
+    validate: true, // Validate params only TURN OFF LATER
+    nonce: null, // TODO: Idk what this is lol
+    ordertype: "market",
+    type: params.action,
+    volume: null, // TODO: Get account value. This is in BASE CURRENCY
+    pair: `${params.symbol}USD`,
+    "close[ordertype]": "stop-loss",
+    "close[price]": null, // TODO: Calc with current price and params.stopLoss
+  }
+}
+const createTradeRequest = (params: TradeParams): unknown => {
+  // Check which exhacnge to use
+  // Build body through that
+  const createBody: Record<ValidExchange, (params: TradeParams) => unknown> = {
+    'Kraken': createKrakenRequest
+  }
+
+  return createBody[params.exchange](params)
+}
+
+const executeTrade = (params: TradeParams) => {
+  const requestBody = createTradeRequest(params)
+  // post order
 }
 
 app.post('/api/trade', (req: Request, res: Response) => {
 
   const reqBody = req.body.json()
   const params = validateTradeParams(reqBody)
+
   if (!params) {
     res.status(400).json({ message: 'Invalid body', data: req.body });
     return;
   }
-
   res.status(201).json({ message: 'Success', data: req.body });
+
+  executeTrade(params)
 
 });
 
